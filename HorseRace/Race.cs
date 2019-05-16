c8b62ca836b3aeed463e0598a29f63c39ec8a0ef
Ara Khechoyan <ara.khechoyan@gmail.com>
2019-05-16 23:49:37 +0400

    FIrst commit

---
+++ HorseRace/Race.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    public class Race
+    {
+        private List<Runner> _runners;
+
+        public double CalculateMargin(List<Runner> runners)
+        {
+            return runners.Sum(x => x.DecimalPrice);
+        }
+    }
+}
---
+++ HorseRace/Runner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    public class Runner
+    {
+        private string _name;
+        private int _x;
+        private int _y;
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string FractionalPrice
+        {
+            get { return $"{_x}/{_y}"; }
+        }
+
+        public double DecimalPrice
+        {
+            get
+            {
+                return 100 / (1 + (double)_x / (double)_y);
+            }
+        }
+    }
+}
