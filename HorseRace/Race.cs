6e6bc6b45243e56816b78d17dcde1f07bd424d8d
Ara Khechoyan <ara.khechoyan@gmail.com>
2019-05-20 23:59:00 +0400

    Some code formatting

--- HorseRace/Race.cs
+++ HorseRace/Race.cs
@@ -9,24 +9,18 @@ namespace HorseRace
     public class Race
     {
         static readonly Random rnd = new Random();
-        private List<Runner> _runners;
-        private double margin;
 
         public Race()
         {
-            _runners = new List<Runner>();
+            Runners = new List<Runner>();
         }
-        public List<Runner> Runners
-        {
-            get { return _runners; }
-            set { _runners = value; }
-        }
+        public List<Runner> Runners { get; set; }
 
-        public double Margin { get { return margin; } }
+        public double Margin { get; private set; }
 
         private void CalculateMargin()
         {
-            margin = _runners.Sum(x => x.DecimalPrice);
+            Margin = Runners.Sum(x => x.DecimalPrice);
         }
 
 
@@ -38,7 +32,7 @@ namespace HorseRace
         /// <returns></returns>
         public Runner CalculateWinner()
         {
-            double sumOfAllChances = _runners.Sum(x => x.Chance(margin));
+            double sumOfAllChances = Runners.Sum(x => x.Chance(Margin));
             double sumU = Math.Ceiling(sumOfAllChances);
             
             var pick = rnd.Next((int)sumU);
@@ -46,13 +40,13 @@ namespace HorseRace
             double sum = 0;
             Runner res = new Runner();
 
-            foreach (var item in _runners)
+            foreach (var item in Runners)
             {
-                sum += item.Chance(margin);
+                sum += item.Chance(Margin);
                 if (sum >= pick)
                 {
                     res = item;
-                    _runners[_runners.IndexOf(res)].winCount++;
+                    Runners[Runners.IndexOf(res)].winCount++;
 
                     break;
                 }
@@ -61,14 +55,14 @@ namespace HorseRace
             return res;
         }
 
-        void DisplayWinner(Runner runner) => Console.WriteLine($"The winner is: {runner.Name}, {runner.FractionalPrice}, Chance: {runner.Chance(margin):0.00}");
+        void DisplayWinner(Runner runner) => Console.WriteLine($"The winner is: {runner.Name}, {runner.FractionalPrice}, Chance: {runner.Chance(Margin):0.00}");
 
         public void RunRace(bool displayWinners = false)
         {
             CalculateMargin();
 
-            if (margin > 140 || margin < 110)
-                throw new ArgumentOutOfRangeException($"'{nameof(margin)}' must be between 110% and 140%");
+            if (Margin > 140 || Margin < 110)
+                throw new ArgumentOutOfRangeException($"'{nameof(Margin)}' must be between 110% and 140%");
             var winner = CalculateWinner();
             if (displayWinners) DisplayWinner(winner);
 
